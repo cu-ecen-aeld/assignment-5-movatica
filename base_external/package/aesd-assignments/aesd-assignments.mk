727cdb391c3b2514232212a401c02521b1ff4c2a
movatica <c0d3@movatica.com>
2023-11-12 22:49:16 +0100

    Update commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '39126eddc57bfb09775dc27c1fe909768eaa1c7d'
+AESD_ASSIGNMENTS_VERSION = 'd21eab3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
